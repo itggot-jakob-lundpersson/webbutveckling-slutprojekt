410798e2b75a599e7c74c5571c6a008ded0bdeae
itggot-jakob-lundpersson <jakob.lundpersson@elev.ga.ntig.se>
2019-05-02 15:41:46 +0200

    Index+css

---
+++ Site/controller.rb
@@ -0,0 +1,5 @@
+layout('layout.html.erb')
+
+ignore(/.sass-cache/)
+ignore(/layout.html.erb/)
+ignore(/\/_.*/)
---
+++ Site/public/JS/main.js
@@ -0,0 +1,6 @@
+function doStuff(event){
+    let menu = document.querySelector("menu")
+    let shade = document.querySelector("shade")
+        menu.classList.toggle("activate")
+        shade.classList.toggle("activate")
+    }
