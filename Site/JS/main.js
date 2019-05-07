02e20ecab557b9b395f10aca306f26f31b73c744
itggot-jakob-lundpersson <jakob.lundpersson@elev.ga.ntig.se>
2019-05-07 14:44:00 +0200

    Flera sidor skapade, layout för andra sidan

--- Site/JS/main.js
+++ Site/JS/main.js
@@ -1,6 +1,6 @@
 function doStuff(event){
     let menu = document.querySelector("menu")
-    let shade = document.querySelector("shade")
+    let shade = document.querySelector(".shade")
         menu.classList.toggle("activate")
         shade.classList.toggle("activate")
     }
